0a4816aa578fe9c4e0c201e1628832096d93f1f2
akashdeeptechalphonic <akashdeep@techalphonic.com>
2026-01-16 12:52:07 +0530

    fix changes of ssr 2

--- src/app/(general)/clients/create/page.js
+++ src/app/(general)/clients/create/page.js
@@ -2,9 +2,10 @@ import React from 'react'
 import PageHeader from '@/components/shared/pageHeader/PageHeader'
 import ProjectCreateContent from '@/components/ClientCreate/ClientCreateContent'
 import ProjectCreateHeader from '@/components/ClientCreate/ClientCreateHeader'
-
+import { Suspense } from "react";
 const page = () => {
   return (
+    <Suspense fallback={<div>Loading...</div>}>
     <>
       <PageHeader>
         <ProjectCreateHeader />
@@ -14,8 +15,8 @@ const page = () => {
           <ProjectCreateContent />
         </div>
       </div>
-
     </>
+</Suspense>
   )
 }
 
