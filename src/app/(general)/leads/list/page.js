61df159258175814e9a7e4893dcf948c59e2c646
akashdeeptechalphonic <akashdeep@techalphonic.com>
2026-01-19 12:44:43 +0530

    asdfasmd akash 1

--- src/app/(general)/leads/list/page.js
+++ src/app/(general)/leads/list/page.js
@@ -1,9 +1,14 @@
+"use client"
 import React from 'react'
 import PageHeader from '@/components/shared/pageHeader/PageHeader'
 import LeadsHeader from '@/components/leads/LeadsHeader'
-import LeadssTable from '@/components/leads/LeadsTable'
+// import LeadssTable from '@/components/leads/LeadsTable'
 import Footer from '@/components/shared/Footer'
-
+import dynamic from 'next/dynamic'
+const LeadssTable = dynamic(
+  () => import('@/components/leads/LeadsTable'),
+  { ssr: false }
+)
 const page = () => {
     return (
         <>
