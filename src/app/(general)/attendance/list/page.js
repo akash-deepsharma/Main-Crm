340346e52339ae63a53399355bf9da37647cd3d8
akashdeeptechalphonic <akashdeep@techalphonic.com>
2026-01-19 11:52:52 +0530

    asdfasmd dsafkjlasd

--- src/app/(general)/attendance/list/page.js
+++ src/app/(general)/attendance/list/page.js
@@ -1,17 +1,48 @@
+// import React from 'react'
+// import PageHeader from '@/components/shared/pageHeader/PageHeader'
+// import ProjectTable from '@/components/ClientAttendanceList/ProjectTable'
+// import ProjectsListHeader from '@/components/ClientList/ClientListHeader'
+
+// const page = () => {
+//     return (
+//         <>
+//             <PageHeader>
+//                 <ProjectsListHeader />
+//             </PageHeader>
+//             <div className='main-content'>
+//                 <div className='row'>
+//                     <ProjectTable />
+//                 </div>
+//             </div>
+//         </>
+//     )
+// }
+
+// export default page
+
+
+
+"use client";
+
 import React from 'react'
 import PageHeader from '@/components/shared/pageHeader/PageHeader'
-// import {ProjectsListHeader} from '@/components/ClientList/ClientListHeader'
-import ProjectTable from '@/components/ClientAttendanceList/ProjectTable'
 import ProjectsListHeader from '@/components/ClientList/ClientListHeader'
+import dynamic from 'next/dynamic'
 
-const page = () => {
+const ProjectTable = dynamic(
+  () => import('@/components/ClientAttendanceList/ProjectTable'),
+  { ssr: false }
+)
+
+const Page = () => {
     return (
         <>
             <PageHeader>
                 <ProjectsListHeader />
             </PageHeader>
-            <div className='main-content'>
-                <div className='row'>
+
+            <div className="main-content">
+                <div className="row">
                     <ProjectTable />
                 </div>
             </div>
@@ -19,4 +50,4 @@ const page = () => {
     )
 }
 
-export default page
+export default Page
