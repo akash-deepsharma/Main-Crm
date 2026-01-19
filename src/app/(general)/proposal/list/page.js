b71a917ef2a8fda83766dfd80d8a165bb8eb175f
akashdeeptechalphonic <akashdeep@techalphonic.com>
2026-01-19 13:13:54 +0530

    asdfasmd akash 2

--- src/app/(general)/proposal/list/page.js
+++ src/app/(general)/proposal/list/page.js
@@ -1,16 +1,17 @@
-// "use client"
+"use client"
 import React from 'react'
-import ProposalTable from '@/components/proposal/ProposalTable'
+// import ProposalTable from '@/components/proposal/ProposalTable'
 import ProposalHeadr from '@/components/proposal/ProposalHeadr'
 import PageHeader from '@/components/shared/pageHeader/PageHeader'
 import ProposalHeaderContent from '@/components/proposal/ProposalHeaderContent'
 import Footer from '@/components/shared/Footer'
-// import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic'
 
-// const ProposalTable = dynamic(
-//   () => import('@/components/components/proposal/ProposalTable'),
-//   { ssr: false }
-// )
+const ProposalTable = dynamic(
+  () => import('@/components/proposal/ProposalTable'),
+  { ssr: false }
+)
+
 const page = () => {
     return (
         <>
