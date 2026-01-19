fce6724616776c86de0e643019e14dd1676923b3
akashdeeptechalphonic <akashdeep@techalphonic.com>
2026-01-19 11:59:44 +0530

    asdfasmd dsafkjlasd

--- src/app/(general)/clients/list/page.js
+++ src/app/(general)/clients/list/page.js
@@ -1,7 +1,12 @@
-import ClientList from '@/components/ClientCreate/ClientList'
+"use client"
+// import ClientList from '@/components/ClientCreate/ClientList'
 import React, { Suspense } from 'react'
+import dynamic from 'next/dynamic'
 
-
+const ClientList = dynamic(
+  () => import('@/components/ClientCreate/ClientList'),
+  { ssr: false }
+)
 const page = () => {
     return (
         <Suspense fallback={<div>Loading...</div>}>
